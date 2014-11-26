b43a6a712f817b28cf42b3efad2d662fb567fc61
haoboyi <hao.boyi@topka.cn>
2014-11-26 17:47:15 +0800

    修改上一版随机排序BUG 优化实现随机写入方式

--- other_tool/audiolist.py
+++ other_tool/audiolist.py
@@ -28,8 +28,10 @@ def crerandomaudiolist():
             _files.append(os.path.join(root, f+"\r\n"))
 
     with open(os.path.join(path, "audio.list"), "ab") as of:
-        for i in random.sample(range(0, len(_files)-1), len(_files)):
-            of.write(_files[i])
+        # for i in random.sample(range(0, len(_files)), len(_files)):   ## V1.0
+        # of.write(_files[i])
+
+        of.write('\r\n'.join(random.sample(_files, len(_files))))  # # V2.0
 
 ## exec
 crerandomaudiolist()
